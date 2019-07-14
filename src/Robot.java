db03d899049c2c86f875c1339cfc6ab1abb36842
SophYo <sophiya.dimitrova@gmail.com>
2019-07-14 23:25:12 +0300

    logic added

--- src/Robot.java
+++ src/Robot.java
@@ -1,4 +1,18 @@
 public class Robot implements Identifiable {
+    private String name;
+    private String id;
+    private String model;
+
+    public Robot(String name,String id, String model) {
+        this.name = name;
+        this.id = id;
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
     @Override
     public String getId() {
         return null;
