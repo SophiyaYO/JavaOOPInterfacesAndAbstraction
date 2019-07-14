68672d6d88e7f1f1d3b58fc51f2e31ffdd326e54
SophYo <sophiya.dimitrova@gmail.com>
2019-07-14 16:09:22 +0300

    classes Chinese, European and Bulgarian- added implements from Person, added field and constructor

--- src/Bulgarian.java
+++ src/Bulgarian.java
@@ -1,2 +1,17 @@
 public class Bulgarian implements Person {
+    private String name;
+
+    public Bulgarian(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String sayHello() {
+        return null;
+    }
 }
--- src/Chinese.java
+++ src/Chinese.java
@@ -1,2 +1,18 @@
 public class Chinese implements Person{
+
+    private String name;
+
+    public Chinese(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String sayHello() {
+        return null;
+    }
 }
