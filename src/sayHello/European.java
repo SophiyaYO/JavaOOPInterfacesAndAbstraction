50cf44f8f16628d526179b40935f9b58f1f9aaa9
SophYo <sophiya.dimitrova@gmail.com>
2019-07-14 22:55:09 +0300

    changed to extend BasePerson and changed European()

--- src/sayHello/European.java
+++ src/sayHello/European.java
@@ -1,10 +1,10 @@
 package sayHello;
 
-public class European implements Person {
+public class European extends BasePerson {
     private String name;
 
     public European(String name) {
-        this.name = name;
+        super(name);
     }
 
     @Override
