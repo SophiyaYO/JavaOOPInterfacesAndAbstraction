7bf109df4dad8a755fb3b480b7c6778befa34ffe
SophYo <sophiya.dimitrova@gmail.com>
2019-07-15 18:46:01 +0300

    added some implementation

--- src/Ferrari/Ferrari.java
+++ src/Ferrari/Ferrari.java
@@ -1,4 +1,33 @@
 package Ferrari;
 
-public class Ferrari implements Car {
+import java.time.Month;
+
+public abstract class Ferrari implements Car {
+    private String MODEL = "488-Spider";
+
+    private String driverName;
+
+    public Ferrari(String driverName) {
+        this.driverName = driverName;
+
+    }
+
+    @Override
+    public String brakes() {
+        return "Brakes!";
+    }
+
+    @Override
+    public String gas() {
+        return "Zadu6avam sA!";
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s/%s/%s",
+                MODEL,
+                this.brakes(),
+                this.gas(),
+                this.driverName);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,32 @@
-import borderControl.Citizen;
-import borderControl.Identifiable;
-import borderControl.Robot;
+import Ferrari.Car;
+import Ferrari.Ferrari;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-            List<Identifiable> all = new ArrayList<>();
-            String input;
-            while (!"End".equals(input = scanner.nextLine())) {
-                String[] tokens = input.split("\\s+");
+        String name = scanner.nextLine();
 
-                if (tokens.length == 3) {
-                    Citizen possibleHuman =
-                            new Citizen(tokens[0], Integer.parseInt(tokens[1]), tokens[2]);
-
-                    all.add(possibleHuman);
-                } else {
-                    Robot possibleRobot = new Robot(tokens[0], tokens[1]);
-
-                    all.add(possibleRobot);
-                }
+        Ferrari newCar = new Ferrari(name) {
+            @Override
+            public String brakes() {
+                return super.brakes();
             }
 
-            String lastDigits = scanner.nextLine();
-
-            for (Identifiable human : all) {
-                if (human.getId().substring(human.getId().length() - lastDigits.length()).equals(lastDigits)) {
-                    System.out.println(human.getId());
-                }
+            @Override
+            public String gas() {
+                return super.gas();
             }
+
+            @Override
+            public String toString() {
+                return super.toString();
+            }
+        };
+
+        System.out.println(newCar.toString());
     }
 }
