e2d59c1e4a156b4669d1a63bd35aaf171f6ef77b
SophYo <sophiya.dimitrova@gmail.com>
2019-07-16 13:27:48 +0300

    created and wanted code written

---
+++ src/defineAnInterfacePerson/Pet.java
@@ -0,0 +1,19 @@
+package defineAnInterfacePerson;
+
+public class Pet {
+    private String name;
+    private String birthDate;
+
+    public Pet(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+}
