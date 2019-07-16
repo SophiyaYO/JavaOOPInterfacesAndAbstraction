480eab7b85e9eceb3396c438ad90b963e9bdfa36
SophYo <sophiya.dimitrova@gmail.com>
2019-07-16 21:48:19 +0300

    problem 6 creating hierarchy of classes and interfaces

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import militaryElite.PrivateImpl;
+import militaryElite.interfaces.Soldier;
+
 public class Main {
     public static void main(String[] args) {
+        Soldier soldier =
+                new PrivateImpl(1, "piju", "milenov", 23);
 
-
+        System.out.println(soldier);
     }
 }
 
---
+++ src/militaryElite/LieutenantGeneralImpl.java
@@ -0,0 +1,31 @@
+package militaryElite;
+
+import militaryElite.interfaces.LieutenantGeneral;
+import militaryElite.interfaces.Private;
+
+import java.util.*;
+
+public class LieutenantGeneralImpl extends PrivateImpl implements LieutenantGeneral {
+    private Set<Private> privates ;
+
+    public LieutenantGeneralImpl(int id, String firstName, String lastName, double salary) {
+        super(id, firstName, lastName, salary);
+        this.privates = new TreeSet<>(
+                (first, second) -> second.getId() - first.getId());
+    }
+
+    @Override
+    public void addPrivate(Private soldier) {
+        this.privates.add(soldier);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(super.toString());
+        builder.append(System.lineSeparator())
+                .append("Privates:")
+                .append(System.lineSeparator());
+
+        return builder.toString().trim();
+    }
+}
